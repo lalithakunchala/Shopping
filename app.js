a6272643a07b2771698a3edd3aa1723e904a43f6
Lalitha Kunchala <lalithakunchala@gmail.com>
2020-09-07 17:04:00 +0530

    file updated

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-const mongouri = require('./config.js')
+const MONGODB_URI = require('./config')
 
 const mongoose = require("mongoose");
 //local
 // mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 //remote 
-mongoose.connect(process.env.MONGODB_URI|| mongouri, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI|| MONGODB_URI, {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error', function(){
--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const mongouri = 'mongodb+srv://mongo_lalitha:lalitha@89@mongooseappcluster.leuky.mongodb.net/mongooseAppDB'
+const MONGODB_URI = 'mongodb+srv://mongo_lalitha:lalitha@89@mongooseappcluster.leuky.mongodb.net/mongooseAppDB';
 
